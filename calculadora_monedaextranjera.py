9cec1d337b2b83ca93d906d452829408b3828c92
Agustín <111393311+AgusVaccaro@users.noreply.github.com>
2023-08-10 19:42:24 -0300

    Creacion de calculadora_monedaextranjera.py
    Se crea el archivo calculadora_monedaextranjera.py, en el cual se desarrollan las funciones que va a llevar a cabo el programa.

---
+++ calculadora_monedaextranjera.py
@@ -0,0 +1,29 @@
+def convertir_a_dolar(peso):
+    tasa_cambio = 0.0041  # Tasa de cambio de peso argentino a dólar estadounidense
+    dolar = peso * tasa_cambio
+    return dolar
+
+def convertir_a_euro(peso):
+    tasa_cambio = 0.0038  # Tasa de cambio de peso argentino a euro
+    euro = peso * tasa_cambio
+    return euro
+
+def convertir_a_reales(peso):
+    tasa_cambio = 0.020  # Tasa de cambio de peso argentino a reales brasileros
+    reales = peso * tasa_cambio
+    return reales
+
+def convertir_a_libras(peso):
+    tasa_cambio = 0.0033  # Tasa de cambio de peso argentino a libras esterlinas
+    libras = peso * tasa_cambio
+    return libras
+
+def convertir_a_yen(peso):
+    tasa_cambio = 0.58  # Tasa de cambio de peso argentino a yen japonés
+    yen = peso * tasa_cambio
+    return yen
+
+def convertir_a_guarani(peso):
+    tasa_cambio = 29.73  # Tasa de cambio de peso argentino a guaraní paraguayo
+    guarani = peso * tasa_cambio
+    return guarani
